2dbcb82b51cfd5a0f881a5075cc439634e5622ef
Jesus Cuellar <jesusacloera@gmail.com>
2019-08-05 15:25:13 -0500

    Script 00

--- Clase 0/Clase_0.R
+++ Clase 0/Clase_0.R
@@ -1,4 +1,28 @@
-# Jesus Alberto Cuellar Loera
+# Jesús Alberto Cuéllar Loera
 #05/08/2019
 # Clase 0
+
+
+# Pasos básicos -----------------------------------------------------------
+2+2
+a <- 2
+a*a
+
+# importar datos ----------------------------------------------------------
+
+
+diametro <- c(12,8.6,9.2,7.7,12.9,11.7,9.7,14.2,
+              11.8,14.3,12.5)
+
+# Tendencia central -------------------------------------------------------
+
+
+mean(diametro)
+median(diametro)
+
+# Dispersión --------------------------------------------------------------
+
+
+sd(diametro)
+var(diametro)
 
