927d3483fa91881891940d9b9239da73c75a1ec4
Jesus Cuellar <jesusacloera@gmail.com>
2019-08-06 10:08:53 -0500

    Tarea 1 Jesus Cuellar

--- Tareas/Tarea1JesusCuellar.R
+++ Tareas/Tarea1JesusCuellar.R
@@ -14,19 +14,19 @@ head(conjunto)
 
 H.media <- subset(conjunto, Altura <= mean(conjunto$Altura))
 
-H.16 <- subset(conjunto, Altura < 16.5)
+H16 <- subset(conjunto, Altura < 16.5)
 
 
 # Subset de vecinos ----------------------------------------------------------
 
-Vecinos.3 <- subset(conjunto, Vecinos <= 3)
-Vecinos.4 <- subset(conjunto, Vecinos > 4)
+Vecinos3 <- subset(conjunto, Vecinos <= 3)
+Vecinos4 <- subset(conjunto, Vecinos > 4)
 
 
 # Subset de diametro ---------------------------------------------------------
 
 DBHmedia <- subset(conjunto, Diametro < mean(conjunto$Diametro))
-DBH.16  <- subset(conjunto, Diametro > 16)
+DBH16  <- subset(conjunto, Diametro > 16)
 
 
 # Subset de especie----------------------------------------------------------
@@ -35,11 +35,11 @@ DBH.16  <- subset(conjunto, Diametro > 16)
 EspCedro <- conjunto[(conjunto$Especie == "C"),]
 EspRestante <- conjunto[!(conjunto$Especie == "C"),]
 
-DiamCedro16.9 <- subset(EspCedro, Diametro <= 16.9)
-AltCedro18.5 <- subset(EspCedro, Altura > 18.5)
+DiamCedro16 <- subset(EspCedro, Diametro <= 16.9)
+AltCedro18 <- subset(EspCedro, Altura > 18.5)
 
-DiamRestante16.9 <- subset(EspRestante, Diametro <= 16.9)
-AltRestante18.5 <- subset(EspRestante, Altura > 18.5)
+DiamRestante16 <- subset(EspRestante, Diametro <= 16.9)
+AltRestante18 <- subset(EspRestante, Altura > 18.5)
 
 
 
@@ -47,21 +47,21 @@ AltRestante18.5 <- subset(EspRestante, Altura > 18.5)
 
 hist(conjunto$Altura, col="green", main="Altura")
 hist(H.media$Altura, col="lightblue", main="Alturas <= Media")
-hist(H.16$Altura, col="aquamarine", main="Alturas < 16.5")
+hist(H16$Altura, col="aquamarine", main="Alturas < 16.5")
 
 
 # Histogramas vecinos -----------------------------------------------------
 
 
 hist(conjunto$Vecinos, col="green", main="Altura")
-hist(Vecinos.3$Vecinos, col="lightblue", main="Alturas <= Media")
-hist(Vecinos.4$Vecinos, col="aquamarine", main="Alturas < 16.5")
+hist(Vecinos3$Vecinos, col="lightblue", main="Alturas <= Media")
+hist(Vecinos4$Vecinos, col="aquamarine", main="Alturas < 16.5")
 
 # histogramas diametro ----------------------------------------------------
 
 hist(conjunto$Diametro, col="green", main="Altura")
 hist(DBHmedia$Diametro, col="lightblue", main="Alturas <= Media")
-hist(DBH.16$Diametro, col="aquamarine", main="Alturas < 16.5")
+hist(DBH16$Diametro, col="aquamarine", main="Alturas < 16.5")
 
 # estadisticas basicas Altura ---------------------------------------------
 
@@ -71,19 +71,19 @@ sd(conjunto$Altura)
 mean(H.media$Altura)
 sd(H.media$Altura)
 
-mean(H.16$Altura)
-sd(H.16$Altura)
+mean(H16$Altura)
+sd(H16$Altura)
 
 # estadisticas basicas vecinos --------------------------------------------
 
 mean(conjunto$Vecinos)
 sd(conjunto$Vecinos)
 
-mean(Vecinos.3$Vecinos)
-sd(Vecinos.3$Vecinos)
+mean(Vecinos3$Vecinos)
+sd(Vecinos3$Vecinos)
 
-mean(Vecinos.4$Vecinos)
-sd(Vecinos.4$Vecinos)
+mean(Vecinos4$Vecinos)
+sd(Vecinos4$Vecinos)
 
 # estadisticas basicas diametro -------------------------------------------
 
@@ -93,6 +93,6 @@ sd(conjunto$Diametro)
 mean(DBHmedia$Diametro)
 sd(DBHmedia$Diametro)
 
-mean(DBH.16$Diametro)
-sd(DBH.16$Diametro)
+mean(DBH16$Diametro)
+sd(DBH16$Diametro)
 
